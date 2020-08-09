0d38c976b690f4d23ad8ab1804526fc5344b3697
Nic Fisher <n.f@live.com.au>
2020-08-09 14:40:05 +1000

    change AssignUser init to single line

--- lib/todo_web/plugs/assign_user.ex
+++ lib/todo_web/plugs/assign_user.ex
@@ -1,8 +1,7 @@
 defmodule TodoWeb.Plugs.AssignUser do
   import Plug.Conn
 
-  def init(_params) do
-  end
+  def init(_params), do: nil
 
   def call(conn, _params) do
     assign(conn, :current_user, Guardian.Plug.current_resource(conn))
