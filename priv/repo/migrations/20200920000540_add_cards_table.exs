6470a299d510163c53b72d1812168e10c1c08004
Nic Fisher <n.f@live.com.au>
2020-09-20 10:49:48 +1000

    add cards migrations and basic add card button

---
+++ priv/repo/migrations/20200920000540_add_cards_table.exs
@@ -0,0 +1,21 @@
+defmodule Todo.Repo.Migrations.AddCardsTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:cards, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :name, :string, null: false
+      add :description, :string
+      add :archived, :boolean, default: false
+      add :due_date, :date
+      add :board_list_id, references(:boards, type: :uuid), null: false
+
+      timestamps()
+    end
+
+    create index("cards", [:board_list_id], where: "archived = false", name: :cards_board_id_and_non_archived_index)
+
+    create index(:cards, :board_list_id)
+    create index(:cards, :id)
+  end
+end
