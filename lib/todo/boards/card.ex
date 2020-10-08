4117d1d4df5dc98aeaf526b58d21e9b294f5e4f0
Nic Fisher <n.f@live.com.au>
2020-10-09 09:41:57 +1100

    remove board_id from card

--- lib/todo/boards/card.ex
+++ lib/todo/boards/card.ex
@@ -10,7 +10,7 @@ defmodule Todo.Boards.Card do
     field :archived, :boolean, default: false
     field :due_date, :date
 
-    belongs_to :board, Board, foreign_key: :board_id, type: :binary_id
+    # belongs_to :board, Board, foreign_key: :board_id, type: :binary_id
     belongs_to :board_list, BoardList, foreign_key: :board_list_id, type: :binary_id
 
     timestamps()
@@ -19,7 +19,7 @@ defmodule Todo.Boards.Card do
   @doc false
   def changeset(card, attrs) do
     card
-    |> cast(attrs, [:name, :description, :archived, :due_date, :board_id])
+    |> cast(attrs, [:name, :description, :archived, :due_date])
     |> validate_required([:name, :board_id])
   end
 end
